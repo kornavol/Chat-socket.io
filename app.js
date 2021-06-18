e43f8d1cc589a7704a0978ba96a164689c8b4bd3
kornavol <volosh.andrey@gmail.com>
2021-06-18 21:41:03 +0200

    final ver. of training project

--- app.js
+++ app.js
@@ -1,75 +1,79 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const jwt = require('jsonwebtoken')
-
+const express = require('express');
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
 const io = new Server(server);
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 8080;
 
-const jwtKey = 'KLJhoasda(*%^9wqel1'
+app.use(express.static(__dirname + '/public'));
 
-/* To initilize statick page.
-    Both ways are secure but the stcond one more secure
-*/
-// app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
+const usersMap = {}
+const users = [];
+const channels = ['JS', 'React', 'Angular', 'Vue', 'NodeJS', 'C', 'Java'];
 
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "publick/index.html");
-// });
+io.on('connection', (socket) => {
 
-io.on("connection", (socket) => {
+    socket.emit('channels', channels);
 
-    function ticketHandler(nickName) {
-
-        if (!nickName) {
-            nickName = "Guest" + Date.now();
+    //When a user connects, we create a new uniq nick name 
+    //and emmit
+    socket.on('token', token => {
+        const ticketHandler = (nickName) => {
+            if (!nickName) {
+                nickName = 'Guest' + Date.now();
+            }
+            const token = jwt.sign({ nickName }, 'ahguy21367278@#$%$%@wer', { expiresIn: '1w' });
+            const ticket = { token, nickName };
+            console.log(nickName + ' is joind to the chat');
+            users.push(nickName);
+            usersMap[nickName] = socket;
+            return ticket
         }
 
-        const token = jwt.sign({ nickName }, jwtKey, { expiresIn: "1d" })
-        const tiket = {token, nickName }
-        return tiket
-    }
-
-    console.log("a user connected!");
-
-    socket.on("token", (token) => {
         if (token) {
             try {
-                jwt.verify(token, jwtKey, function (fail, decoded) {
-
+                jwt.verify(token, 'ahguy21367278@#$%$%@wer', (fail, decodedPayload) => {
                     if (fail) {
+                        //token is invalid here
                         socket.emit('ticket', ticketHandler());
-                        
-                        console.log('fail', fail);
                     } else {
-                        socket.emit("ticket", ticketHandler(decoded.nickName));
-
-                        console.log(decoded.nickName + "conected");
-                        console.log('token', token);
+                        //we can trust decoded payload here
+                        socket.emit('ticket', ticketHandler(decodedPayload.nickName));
                     }
                 });
             } catch (err) {
                 console.log(err);
+                //TODO: Manage error case on server and client
             }
         } else {
             socket.emit('ticket', ticketHandler());
         }
+        //TODO: Check if the user is active
+        io.emit('users', users);
+
     });
 
-    socket.on("chat message", (message) => {
-        console.log(message);
-        io.emit("chat msg", message);
+
+
+    socket.on('chat message', (envelope) => {
+        if (envelope.user) {
+            //message goes to user
+            socket.emit('chat msg', envelope)
+            usersMap[envelope.user].emit('chat msg', envelope)
+        } else {
+            //message goes to the channel
+            io.emit('chat msg', envelope);
+        }
+        //console.log(message);
+        //io.emit('chat msg', message);
     });
 
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
+    socket.on('disconnect', () => {
+        console.log('a user disconnected!');
     });
 });
 
-server.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+server.listen(port, () => console.log(`Server started on port ${port}`));
--- public/main.js
+++ public/main.js
@@ -1,24 +1,120 @@
 const socket = io();
 
-const clickHandler = (e) => {
-    const msg = document.querySelector("#msg-box").value;
-    socket.emit("chat message", msg);
+const $ = selector => document.querySelector(selector);
+
+const listEnumerable = ['channels', 'users']
+
+const state = {
+    list: 0,
+    users:[],
+    channels:[],
+    activeChannel: 'JS',
+    activeUser: null,
+    usedChannels:{JS:[]}
 };
 
-socket.emit("token", sessionStorage.getItem("token"));
+const switchHandler = () => {
+    //alert(state.list);
+    state.activeUser = null
+    switch (state.list) {
+        case 0:
+            state.list = 1;
+            listManager();
+            break;
+            
+        case 1:
+            state.list = 0;
+            listManager();
+                break;
+    
+        default:
+            alert(`There's problem please try again later`)
+            break;
+    }
+}
 
-socket.on("ticket", (ticket) => {
-    console.log(ticket);
-    document
-    .querySelector("#chat-box")
-    .insertAdjacentHTML("beforeend", `<li> Welcome on board ${ticket.nickName} </li>`);
-    sessionStorage.setItem("token", ticket.token);
+const listClickHandler = (name) => {
+    //alert(name);
+    if (state.list == 0 ) {
+        const preItem = state.activeChannel;
+        state.activeChannel = name;
+        $(`#${preItem}`).classList.remove('active');
+        $(`#${name}`).classList.add('active');
+        if(!state.usedChannels[name]){
+            state.usedChannels[name] = [];
+        }
+        console.log(state.usedChannels);
+        $('#chat-box').innerHTML = '';
+        state.usedChannels[state.activeChannel].forEach( msg => {
+            document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li>${msg}</li>`);
+        });
+    } else if(state.list == 1) {
+        //TODO: Manage for personal messaging
+        state.activeUser = name;
+    }
+}
+
+const listManager = () => {
+    $('#active-list').innerHTML = '';
+    const list = state[listEnumerable[state.list]];
+    list.forEach(item => {
+        $('#active-list').insertAdjacentHTML('beforeend', 
+        `<li id = "${item}" onclick = "listClickHandler('${item}')" class = "list-item">
+            ${item}
+        </li>`);
+
+        if(item == list[list.length-1] && state.list === 0){
+            listClickHandler(state.activeChannel);
+        }
+    });
+}
+
+socket.on('chat msg', envelope => {
+
+    //if envelope.channel == onse of the keys of state.usedChannels
+
+    if(envelope.user) {
+
+        document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li>${envelope.msg}</li>`);
+
+    } else {
+
+        Object.keys(state.usedChannels).forEach( key => {
+            if(envelope.channel == key) {
+                state.usedChannels[key].push(envelope.msg);
+            }
+
+            console.log(state.usedChannels[key]);
+        });
+
+        if (state.activeChannel == envelope.channel) {
+            document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li>${envelope.msg}</li>`);
+        } 
+    }
 });
 
-
-socket.on("chat msg", (msg) => {
-    // let nick = sessionStorage.getItem('nick')
-    document
-    .querySelector("#chat-box")
-    .insertAdjacentHTML("beforeend", `<li> ${msg} </li>`);
+socket.on('users', users => {
+    state.users = users;
+    listManager();
 });
+
+socket.on('channels', channels => {
+    state.channels = channels;
+    listManager();
+});
+
+const clickHandler = () => {
+    let msg = $('#msg-box');
+    socket.emit('chat message', {msg:msg.value, channel:state.activeChannel, user: state.activeUser});
+    msg.value = '';
+}
+
+socket.emit('token', sessionStorage.getItem('token'));
+
+socket.on('ticket', ticket => {
+    document
+    .querySelector('#chat-box')
+    .insertAdjacentHTML("beforeend", `<li>Welcome on board ${ticket.nickName}</li>`);
+    sessionStorage.setItem('token', ticket.token);
+});
+
